d91496dfb15e40bbd9141702ca0c26c37f290349
bha <b.ha@controlexpert.com>
2025-03-18 09:46:29 +0100

    Initial install

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+WORKDIR=$(dirname $(realpath "$0"))
+cd ${WORKDIR}
 
+## Installation
 sudo apt update
-
 sudo apt install -y tmux
+
+## Sourcing
+source ./.bash_miscs
+
+## Estabilising configs
+ln -sv "${WORKDIR}/.tmux.conf" ~
